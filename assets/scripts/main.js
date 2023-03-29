f3db479e6e1633ee79cf26459d00583d26df13c5
Kenny S <35939549+kxs6629@users.noreply.github.com>
2023-03-29 16:32:52 -0400

    Implemented fullPage.js for fullscreen scroll

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,11 @@
-console.log("hello everybody my name is Markiplier");
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+        licenseKey:'gplv3-license',
+		autoScrolling:true,
+		scrollHorizontally: true
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(true);
+});
