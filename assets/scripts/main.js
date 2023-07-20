e46aa46fb15a6c17d1ab4401f998bc02b1f58f2c
Kenny S <35939549+kxs6629@users.noreply.github.com>
2023-07-20 01:44:49 -0400

    Implementing dots for nav progress

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -22,3 +22,40 @@ function navFunction(){
 		links.style.display = "block"
 	}
 }
+
+// (function(){
+// 	let dotCount = 0;
+// 	let prevScroll = 0;
+// 	$(window).scroll(function(){
+// 		let curScroll = $(this).scrollTop();
+// 		if (curScroll > prevScroll){
+// 			dotCount++;
+			// $("#dotList").children()[dotCount].attr("background-color","#ffffff");
+// 		} else {
+// 			dotCount--;
+// 			$("#dotList").children()[dotCount].attr("background-color","#ffffff");
+// 		}
+// 		prevScroll = curScroll;
+// 		console.log(dotCount);
+// 	});
+// }());
+
+let dotCount = 0;
+let curSpan = $(".navDot")[dotCount]
+$('body').on('mousewheel', function(e){
+	if(e.originalEvent.wheelDelta > 0) {
+		console.log('up 3');
+		dotCount--;
+		console.log($(".navDot")[dotCount])
+		curSpan.css("background-color","#ffffff");
+		// $("#dotList").children()[dotCount].attr("background-color","#ffffff");
+		// .attr("background-color","#ffffff")
+	}
+	else {
+		console.log('down 3');
+		dotCount++;
+		console.log($(".navDot")[dotCount])
+		// console.log($("#dotList").children()[dotCount])
+		// $("#dotList").children()[dotCount].attr("background-color","#ffffff")
+	}
+});
