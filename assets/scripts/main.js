a17a43440375697c8a120ead21a37c5880dddc71
Kenny S <35939549+kxs6629@users.noreply.github.com>
2024-10-16 18:55:57 -0400

    Fixed up JS and cleaning up CSS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,50 +2,10 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		//options here
         licenseKey:'gplv3-license',
-		autoScrolling:true,
-		scrollHorizontally: true
+		// autoScrolling:true,
+		navigation: true,
+		anchors: ['welcome','about','experience','project','contact'],
+		menu:"#navMenu",
+		parallax: true
 	});
-
-	//methods
-	$(".navDot:eq(0)").css({"backgroundColor":"#ffffff"});
-	$.fn.fullpage.setAllowScrolling(true);
-	
-});
-
-/**
-	thinking of how to assign each navDot a nav value
-	might just be easier to make the spans a tags for navigation
-	then have the page check which section it's on and update accordingly
-*/
-let navTuple = [
-	[0,"#homeScreen"],
-	[1,"#aboutScreen"],
-	[2,"#experienceScreen"],
-	[3,"#projectScreen"],
-	[4,"#contactScreen"],
-]
-
-/**
-	There's some bugs here atm, but a basic proof of concept exsits
-	Behavior should be reflected based on navigation progress, not just scroll
-	Else this leads to behavior currently exhibted when scrolling faster than fullpage can scroll
-*/
-let dotCount = 0;
-$('body').on('mousewheel', function(e){
-	if(e.originalEvent.wheelDelta > 0) {
-		console.log('up');
-		if(dotCount > 0){
-			$(".navDot:eq("+dotCount+")").css({"backgroundColor":"transparent"});
-			dotCount--;
-			$(".navDot:eq("+dotCount+")").css({"backgroundColor":"#ffffff"});
-		}
-	}
-	else {
-		console.log('down');
-		if(dotCount < 4){
-			$(".navDot:eq("+dotCount+")").css({"backgroundColor":"transparent"});
-			dotCount++;
-			$(".navDot:eq("+dotCount+")").css({"backgroundColor":"#ffffff"});
-		}
-	}
 });
