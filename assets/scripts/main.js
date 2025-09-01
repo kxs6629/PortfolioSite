3e4f336d65d91c78f1cdb0a3ce98d551b92fc0d2
kxs6629 <kxs6629@g.rit.edu>
2025-09-01 19:47:36 -0400

    Prepping for pull to main

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,35 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		//options here
-        licenseKey:'gplv3-license',
-		// autoScrolling:true,
-		navigation: true,
-		anchors: ['welcome','about','experience','project','contact'],
-		menu:"#navMenu",
-		slidesNavigation: true,
-		slidesNavPosition :'bottom',
-		parallax: true
-	});
-});
+let cardDeck = document.getElementsByClassName("skillCard");
+let slideList = document.getElementsByClassName("projectSlide");
+
+let slideCount = 0;
+
+slideList[0].style.display = "inherit"
+
+for (let i = 0; i < cardDeck.length; i++) {
+    cardDeck[i].addEventListener("click",function(){
+        flipCard(this);
+    })
+};
+
+function flipCard(e){
+    let daycare = e.children;
+    for(let i = 0;  i < daycare.length; i++){
+        daycare[i].classList.toggle('hide')
+    }
+}
+
+function showSlide(index){
+    slideList[slideCount].style.display = "none"
+    if(slideCount+index > slideList.length-1){
+        slideCount = 0
+    }
+    else if(slideCount+index < 0){
+        slideCount = slideList.length-1
+    
+    }
+    else{
+        slideCount += index;
+    }
+
+    slideList[slideCount].style.display = "inherit"    
+}
